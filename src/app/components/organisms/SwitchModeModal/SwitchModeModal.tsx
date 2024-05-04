489f9c7f45b9375eba57699f27ee9970c2eae6f7
Iliatsiklauri <tsiklauri.ilia26@gmail.com>
2024-05-04 21:21:24 +0400

    feat(add):add board titles in specific modals

--- src/app/components/organisms/SwitchModeModal/SwitchModeModal.tsx
+++ src/app/components/organisms/SwitchModeModal/SwitchModeModal.tsx
@@ -1,16 +1,33 @@
 import useData from '@/app/utils/useData';
 import React from 'react';
 import ColorSwitch from '../../molecules/ColorSwitch/ColorSwitch';
+import { motion } from 'framer';
+import Image from 'next/image';
 
 export default function SwitchModeModal() {
   const { data, mode, AllBoardsModal, setAllBoardModal, setMode } = useData();
   return (
-    <div
+    <motion.div
+      animate={{
+        y: AllBoardsModal ? 0 : -300,
+        opacity: AllBoardsModal ? 1 : 0,
+      }}
+      transition={{ type: 'spring', damping: 20 }}
       className={`${
         mode ? 'bg-white' : 'bg-DarkGrey'
-      } min-w-[264px] w-[50%] h-[300px] py-4 shadow-[0_10px_20px_0_rgba(54,78,126,0.25)] rounded-lg flex items-start justify-center flex-col gap-4`}
+      } min-w-[264px] w-[50%] py-4 shadow-[0_10px_20px_0_rgba(54,78,126,0.25)] rounded-lg flex items-center justify-center flex-col gap-4`}
     >
+      <div className="self-start w-full pr-6">
+        {data?.map((el) => (
+          <button className="flex  items-center justify-start w-full active:bg-MainPurple active:text-white text-MediumGrey pl-6 gap-3 py-[15px] rounded-tr-[100px] rounded-br-[100px]">
+            {/* TODO: change image to component so we can toggle the fill atribute on button
+              active state */}
+            <Image src={'/icon-board.svg'} alt="board" height={16} width={16} />
+            <p className={` `}>{el.name}</p>
+          </button>
+        ))}
+      </div>
       <ColorSwitch />
-    </div>
+    </motion.div>
   );
 }
