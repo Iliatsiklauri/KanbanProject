3afbdab800831999d5093fcc50c5c37829a8cc8f
Iliatsiklauri <tsiklauri.ilia26@gmail.com>
2024-05-04 21:25:19 +0400

    fix:add key prop in map

--- src/app/components/organisms/SwitchModeModal/SwitchModeModal.tsx
+++ src/app/components/organisms/SwitchModeModal/SwitchModeModal.tsx
@@ -18,8 +18,11 @@ export default function SwitchModeModal() {
       } min-w-[264px] w-[50%] py-4 shadow-[0_10px_20px_0_rgba(54,78,126,0.25)] rounded-lg flex items-center justify-center flex-col gap-4`}
     >
       <div className="self-start w-full pr-6">
-        {data?.map((el) => (
-          <button className="flex  items-center justify-start w-full active:bg-MainPurple active:text-white text-MediumGrey pl-6 gap-3 py-[15px] rounded-tr-[100px] rounded-br-[100px]">
+        {data?.map((el, i) => (
+          <button
+            key={i}
+            className="flex  items-center justify-start w-full active:bg-MainPurple active:text-white text-MediumGrey pl-6 gap-3 py-[15px] rounded-tr-[100px] rounded-br-[100px]"
+          >
             {/* TODO: change image to component so we can toggle the fill atribute on button
               active state */}
             <Image src={'/icon-board.svg'} alt="board" height={16} width={16} />
