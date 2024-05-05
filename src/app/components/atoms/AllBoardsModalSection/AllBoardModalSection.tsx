e3847d3accba2a488f2def131b3f817ad96d7079
Iliatsiklauri <tsiklauri.ilia26@gmail.com>
2024-05-05 22:58:17 +0400

    feat(add):finished all Boards Modal

---
+++ src/app/components/atoms/AllBoardsModalSection/AllBoardModalSection.tsx
@@ -0,0 +1,22 @@
+import { Board } from '@/app/data/types';
+import React from 'react';
+import IconBoard from '../IconBoard/IconBoard';
+import useData from '@/app/utils/useData';
+
+export default function AllBoardModalSection({ el, i }: { el: Board; i: number }) {
+  const { setBoard, board } = useData();
+  return (
+    <button
+      key={i}
+      className={`flex  items-center justify-start w-full  text-MediumGrey pl-6 gap-3 py-[15px] rounded-tr-[100px] rounded-br-[100px] ${
+        el.name === board ? 'bg-MainPurple text-white' : ''
+      }`}
+      onClick={() => {
+        setBoard(el.name);
+      }}
+    >
+      {el.name === board ? <IconBoard fill /> : <IconBoard />}
+      <p className={`font-bold text-[15px]`}>{el.name}</p>
+    </button>
+  );
+}
