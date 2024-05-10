c9644772c014ae048c6d4e211e3351c8184d4f39
Iliatsiklauri <tsiklauri.ilia26@gmail.com>
2024-05-10 21:19:42 +0400

    reset

--- src/app/utils/useData.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react';
-import { GlobalContext } from '../data/types';
-
-export default function useData() {
-  const context = useContext(GlobalContext);
-  if (!context) throw new Error('useData must be used within a GlobalContextProvider');
-
-  return context;
-}
